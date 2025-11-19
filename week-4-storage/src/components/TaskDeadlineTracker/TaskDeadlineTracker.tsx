899bcfa7708319a0d7d1eee745158977f8ddd874
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-11-19 22:44:56 +0100

    Done with TaskDeadlineTracker

--- week-4-storage/src/components/TaskDeadlineTracker/TaskDeadlineTracker.tsx
+++ week-4-storage/src/components/TaskDeadlineTracker/TaskDeadlineTracker.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./TaskDeadlineTracker.css";
 
 type TaskTracker = {
   name: string;
   date: string;
   status: boolean;
+  description: string;
 };
 export const TaskDeadlineTracker = () => {
   const [tasks, setTasks] = useState<TaskTracker[]>(() => {
@@ -15,6 +16,18 @@ export const TaskDeadlineTracker = () => {
   const [name, setName] = useState("");
   const [date, setDate] = useState("");
   const [editing, setEditing] = useState<number | null>(null);
+  const [description, setDescription] = useState("");
+
+  const [tick, setTick] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => setTick((t) => t + 1), 1000);
+    return () => clearInterval(interval);
+  }, []);
+  const saveTask = (updated: TaskTracker[]) => {
+    setTasks(updated);
+    localStorage.setItem("tasks", JSON.stringify(updated));
+  };
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault();
@@ -23,27 +36,24 @@ export const TaskDeadlineTracker = () => {
 
     if (editing !== null) {
       const updated = [...tasks];
-      updated[editing] = { ...updated[editing], name, date };
+      updated[editing] = { ...updated[editing], name, date, description };
       saveTask(updated);
       setEditing(null);
     } else {
-      const newTask = { name, date, status: false };
+      const newTask = { name, date, description, status: false };
       saveTask([...tasks, newTask]);
     }
 
     setName("");
     setDate("");
-  };
-
-  const saveTask = (updated: TaskTracker[]) => {
-    setTasks(updated);
-    localStorage.setItem("tasks", JSON.stringify(updated));
+    setDescription("");
   };
 
   const editTask = (i: number) => {
     const task = tasks[i];
     setName(task.name);
     setDate(task.date);
+    setDescription(task.description);
     setEditing(i);
   };
 
@@ -55,6 +65,26 @@ export const TaskDeadlineTracker = () => {
     const filtered = tasks.filter((_, i) => i !== index);
     saveTask(filtered);
   };
+
+  const toggleStatus = (index: number) => {
+    const updated = [...tasks];
+    updated[index].status = !updated[index].status;
+    saveTask(updated);
+  };
+
+  const getDaysLeft = (taskDate: string) => {
+    const now = new Date().getTime();
+    const target = new Date(taskDate).getTime();
+
+    const diff = target - now;
+    if (diff < 0) return 0;
+
+    return Math.ceil(diff / (1000 * 60 * 60 * 24));
+  };
+
+  const sortedTasks = [...tasks].sort((a, b) => {
+    return new Date(a.date).getTime() - new Date(b.date).getTime();
+  });
 
   return (
     <div className="TaskDeadlineTracker">
@@ -77,25 +107,65 @@ export const TaskDeadlineTracker = () => {
           />
         </div>
 
-        <button type="submit">
+        <div className="TaskContent">
+          <textarea
+            className="TextDescription"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="submitBtn">
           {editing !== null ? "Update Task" : "Add Task"}
         </button>
       </form>
+
       <ul className="tasksList">
-        {tasks.map((task, i) => (
-          <li className="taskItem" key={i}>
-            <h3>{task.name}</h3>
-            <p>{task.date}</p>
-            <div className="actions">
-              <button className="btn editBtn" onClick={() => editTask(i)}>
-                Edit
-              </button>
-              <button onClick={() => deleteTask(i)} className="btn deleteBtn">
-                Delete{" "}
-              </button>
-            </div>
-          </li>
-        ))}
+        {sortedTasks.map((task) => {
+          const daysLeft = getDaysLeft(task.date);
+          const isOverdue = daysLeft === 0 && new Date(task.date) < new Date();
+          const index = tasks.indexOf(task); // original index
+
+          return (
+            <li
+              className={`taskItem ${isOverdue ? "expired" : ""} ${
+                task.status ? "active" : ""
+              }`}
+              key={index}
+            >
+              <div className="taskDescription">
+                <div className="taskDate">
+                  <h3>{task.name}</h3>
+                  <p>{task.date}</p>
+                </div>
+
+                {!task.status && !isOverdue && (
+                  <p className="countdown">{daysLeft} days left</p>
+                )}
+                {isOverdue && <p className="expiredText">Expired</p>}
+
+                <p>{task.description}</p>
+              </div>
+              <div className="actions">
+                <button className="btn editBtn" onClick={() => editTask(index)}>
+                  Edit
+                </button>
+                <button
+                  onClick={() => deleteTask(index)}
+                  className="btn deleteBtn"
+                >
+                  Delete
+                </button>
+                <button
+                  onClick={() => toggleStatus(index)}
+                  className="btn statusBtn"
+                >
+                  {task.status ? "Undo" : "Done"}
+                </button>
+              </div>
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
