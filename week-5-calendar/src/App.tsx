4d5cc97f1af9230aafbd64a397258e987faf1238
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-12-01 19:18:06 +0100

    Done with Simple Calendar

--- week-5-calendar/src/App.tsx
+++ week-5-calendar/src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { SimpleCalendar } from "./components/SimpleCalendar/SimpleCalendar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      {/* Simple Calendar */}
+      <div className="section">
+        <h1>Simple Calendar </h1>
+        <SimpleCalendar />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+
+      {/* Habit Tracker */}
+      <div className="section">
+        <h1>Habit Tracker</h1>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      {/* Work Hours Calculator*/}
+      <div className="section">
+        <h1> Work Hours Calculator</h1>
+      </div>
+
+      {/* React Calendar Integration */}
+      <div className="section">
+        <h1>React Calendar Integration</h1>
+      </div>
+
+      {/*Calendar with Events */}
+      <div className="section">
+        <h1>Calendar with Events</h1>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- week-5-calendar/src/components/SimpleCalendar/SimpleCalendar.tsx
+++ week-5-calendar/src/components/SimpleCalendar/SimpleCalendar.tsx
@@ -1,5 +1,99 @@
+import { useState } from "react";
 import "./SimpleCalendar.css";
+const months = [
+  { name: "January", days: 31 },
+  { name: "February", days: 28 },
+  { name: "March", days: 31 },
+  { name: "April", days: 30 },
+  { name: "May", days: 31 },
+  { name: "June", days: 30 },
+  { name: "July", days: 31 },
+  { name: "August", days: 31 },
+  { name: "September", days: 30 },
+  { name: "October", days: 31 },
+  { name: "november", days: 30 },
+  { name: "December", days: 31 },
+];
 
+const weekDays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
 export const SimpleCalendar = () => {
-  return <div>SimpleCalendar</div>;
+  const today = new Date();
+
+  const [year, setYear] = useState(today.getFullYear());
+  const [monthIndex, setMonthIndex] = useState(today.getMonth());
+
+  const getDaysInMonth = () => {
+    if (monthIndex === 1) {
+      return year % 4 === 0 ? 29 : 28;
+    }
+
+    return months[monthIndex].days;
+  };
+
+  const buildCalendarGrid = () => {
+    const totalDays = getDaysInMonth();
+    const grid = [];
+
+    let firstDay = new Date(year, monthIndex, 1).getDay();
+    firstDay = firstDay === 0 ? 6 : firstDay - 1;
+
+    for (let i = 0; i < firstDay; i++) {
+      grid.push(null);
+    }
+
+    for (let d = 1; d <= totalDays; d++) {
+      grid.push(d);
+    }
+
+    while (grid.length % 7 !== 0) {
+      grid.push(null);
+    }
+
+    return grid;
+  };
+
+  const prevMonth = () => {
+    if (monthIndex === 0) {
+      setMonthIndex(11);
+      setYear(year - 1);
+    } else {
+      setMonthIndex(monthIndex - 1);
+    }
+  };
+
+  const grid = buildCalendarGrid();
+
+  const nextMonth = () => {
+    if (monthIndex === 11) {
+      setMonthIndex(0);
+      setYear(year + 1);
+    } else {
+      setMonthIndex(monthIndex + 1);
+    }
+  };
+  return (
+    <div className="SimpleCalendar">
+      <header className="header">
+        <button onClick={prevMonth}>◀</button>
+        {months[monthIndex].name} {year}
+        <button onClick={nextMonth}>▶</button>
+      </header>
+
+      <div className="weekDays">
+        {weekDays.map((day) => (
+          <div className="weekday" key={day}>
+            {day}
+          </div>
+        ))}
+      </div>
+
+      <div className="grid">
+        {grid.map((cell, i) => (
+          <div className="cell" key={i}>
+            {cell}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
